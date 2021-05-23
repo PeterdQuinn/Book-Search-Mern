9d4dd7b746ce7aaac04b50554a3aedbbbb6e191a
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-23 03:12:44 -0700

    middleware. auth.js

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,34 +1,36 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+
+const jwt = require('jsonwebtoken');
+
+
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+   
     let token = req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return res.status(400).json({ message: 'You shall not pass!' });
     }
-
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(400).json({ message: 'Not a good token boss!' });
     }
-
-    // send to next endpoint
     next();
   },
   signToken: function ({ username, email, _id }) {
@@ -37,3 +39,6 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+
+
